b7309fd31adf1407026bc75f3afdb844655d5064
Your Name <you@example.com>
2022-04-09 19:42:23 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -71,7 +71,7 @@ void enlarge(HashMap * map) {
             long hashvalue = hash(head->key, map->capacity);
             printf("[I]: hash: %ld\n", hashvalue);
             if (!map->buckets[hashvalue]) {
-                eraseMap(map, pair.key);
+                eraseMap(map, pair.value);
                 insertMap(map, pair.key, pair.value);
 
             }
